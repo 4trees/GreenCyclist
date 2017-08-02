2605844fd937c0b3a7fb9f12e09347aa44a3ca33
Liuhuaying <go4trees@gmail.com>
2017-08-02 00:45:46 -0400

    see trees

--- beautiful/script.js
+++ beautiful/script.js
@@ -11,32 +11,45 @@ var timePoint = [
 	]
 console.log('timePeriod: ',timePoint)
 var seasonPoint = [
-	{name:'spring',range:[3,4,5]},
-	{name:'summer',range:[6,7,8]},
-	{name:'autumn',range:[9,10,11]},
-	{name:'winter',range:[12,1,2]}]
+	{name:'spring',range:[3,4,5],order:1},
+	{name:'summer',range:[6,7,8],order:2},
+	{name:'autumn',range:[9,10,11],order:3},
+	{name:'winter',range:[12,1,2],order:4}]
 
 var	file = '../beautiful/data/trip.csv';
+var w = window.innerWidth,
+	h = window.innerHeight,
+	eachH = h * .8
+
+var random = d3.scaleQuantize()
+  .domain([0,1])
+  .range([1,2,3,4]);
 
 d3.queue()
   .defer(d3.csv,file,parse)
   .await(dataloaded);
 
 function dataloaded(err, trips){
-trips.sort(function(x, y){
-   return d3.ascending(timePoint.find(function(d){return d.name == x.point}).order, timePoint.find(function(d){return d.name == y.point}).order);
-})
 console.log('Raw data: ',trips)
 const tripsByseason = d3.nest()
-	.key(function(d){return d.point})
-	.key(function(d){return d.season})
-	.rollup(function(l){
-		let totalDuration = d3.sum(l,function(el){return el.duration})
-		let timeSecond = timePoint.find(function(el){return el.name == l[0].point}).seconds
+	.key(d => d.point)
+	.key(d => d.season)
+	.rollup(l => {
+		let totalDuration = d3.sum(l,el => el.duration)
+		let timeSecond = timePoint.find(el => el.name == l[0].point).seconds
+		let treeCount = Math.floor(totalDuration * 0.00267 * 411 / (16000 / 24 / 60 / 60 * timeSecond) / 100)
+		let trees = []
+		for (i=0;i<treeCount;i++){
+			trees.push({img:random(Math.random()),x:Math.random(),y:Math.random(),season:l[0].season})
+		}
 		return {
-		rides:l.length,
+		time:l[0].point,
+		season: l[0].season,
+		rides:l,
 		totalDuration:totalDuration,
-		treeCount: totalDuration * 0.00267 * 411 / (16000 / 24 / 60 / 60 * timeSecond)}})
+		trees: trees
+		}
+	})
 	.entries(trips)
 console.log('nested: ',tripsByseason)
 console.log('formulation of tree count: totalDuration * 0.00267 * 411 / (16000 / 24 / 60 / 60 * secondsOfThisTimeperiod)')
@@ -44,22 +57,52 @@ draw(tripsByseason)
 
 }
 function draw(data){
+
 // Duration * 0.00267 * 411 = total grams | 20 km/h (12.4274 mile/hour: 0.00345 mile/sec)
 // 16000 / 24 / 60 / 60  = grams per second
+var svg = d3.select('#canvas').append('svg')
+	.attr('width',w).attr('height',eachH * timePoint.length)
+//update
+var updateTimepoint = svg.selectAll('.timepoint').data(data.sort((x, y) => {
+   return d3.ascending(timePoint.find(d => d.name == x.key).order, timePoint.find(d => d.name == y.key).order);
+}))
+var enterTimepoint = updateTimepoint.enter().append('g').attr('class',d => `timepoint ${slug(d.key)}`)
+	.attr('transform',(d,i) =>`translate(0,${i * eachH})`)
+var updateSeason = enterTimepoint.selectAll('.season').data(d => d.values.sort((x, y) => {
+   return d3.ascending(seasonPoint.find(d => d.name == x.key).order, seasonPoint.find(d => d.name == y.key).order);
+}))
+var enterSeason = updateSeason.enter().append('g').attr('class',d => `season ${d.key}`)
+	.attr('transform',(d,i) => `translate(${i * w / 4},0)`)
+
+var updatetrees = enterSeason.append('g').attr('class','trees').selectAll('.tree').data(d => d.value.trees),
+	entertrees = updatetrees.enter()
+		.append('svg:image')
+    	.attr('class','tree')
+    	.attr('xlink:href',d => `../beautiful/img/${d.season}${d.img}.svg`)
+    	.attr('transform',d => `translate(${d.x * w / 4},${d.y * eachH * .7})`)
+// var updaterider = enterSeason.append('g').attr('class','riders').attr('transform',`translate(0,${eachH * .7})`).selectAll('.rider').data((d, i) => console.log(d.value,i,d.key)),
+// enterrider = updaterider.enter().append('g').attr('class','rider');
+// enterrider.append('circle').attr('r',3).attr('transform', `translate(${Math.random() * w /4},${eachH * .7 * Math.random()})`)
+var timelabelContainer = enterSeason.append('g').attr('class','timelabel').attr('transform',`translate(${w / 8},${eachH * .9})`)
+timelabelContainer.append('text').html(d => `<tspan x=0 dy=20>${d.value.season}</tspan><tspan x=0 dy=20>${d.value.time}</tspan>`)
+
 }
 
+function slug(data){
+	let newdata = data.replace(' ','-')
+	return newdata
+}
 
 function parse(d){
 var date = new Date(d['starttime'].replace(/-/g, "/"));
 var refDate = new Date(date)
 var mon = date.getMonth() + 1;
 
-var point = timePoint.find(function(t){
+var point = timePoint.find(t => {
 	var start = refDate.setHours(t.time[0][0],t.time[0][1],0)
 	var end = refDate.setHours(t.time[1][0],t.time[1][1],59)
 	return date >= new Date(start) && date <= new Date(end)}).name;
-var season = seasonPoint.find(function(s){
-	return s.range.includes(mon)}).name;
+var season = seasonPoint.find(s => s.range.includes(mon)).name;
 return{
     duration: +d.tripduration,
     starttime: date.getTime(),
